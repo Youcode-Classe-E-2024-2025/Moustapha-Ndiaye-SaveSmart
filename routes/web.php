09979dcf5712eb621849ba52a280629044c53cee
amdymstfa <amdymoustapha011@gmail.com>
2025-02-27 12:45:11 +0000

    done: get login and register page via routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// publics routes 
+Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+
