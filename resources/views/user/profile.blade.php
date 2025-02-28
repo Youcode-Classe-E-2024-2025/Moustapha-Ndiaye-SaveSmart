0ec00091c8b9268eea29412b202c1af558bca90e
amdymstfa <amdymoustapha011@gmail.com>
2025-02-28 11:15:34 +0000

    done: logout

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -70,6 +70,8 @@
             </svg>
             <span class="hidden sm:inline">Add a member</span>
           </button>
+          <form action="{{url('logout')}}" method="Post">
+            @csrf
           <button class="text-gray-600 hover:text-red-600 transition-colors flex items-center gap-2 p-2 rounded-lg hover:bg-gray-100">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
               <path fill-rule="evenodd" d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z" clip-rule="evenodd" />
@@ -77,6 +79,7 @@
             </svg>
             <span class="hidden sm:inline">Logout</span>
           </button>
+          </form>
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('register', [AuthController::class, 'showRegisterForm'])->name('regis
 Route::post('register', [AuthController::class, 'register']);
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
 
 // auth routes 
 Route::middleware(['auth'])->group(function (){
